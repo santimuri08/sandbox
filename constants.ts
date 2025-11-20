f0c50a72a61c0f326ad671dce5d44e62794c1a4f
santimuri08 <115190223+santimuri08@users.noreply.github.com>
2025-11-19 20:01:09 -0500

    Add files via upload

---
+++ constants.ts
@@ -0,0 +1,11 @@
+export const HALF = 0.5;
+export const TOAST_DURATION = 5000;
+export const ONE_HUNDRED_PERCENT = 100;
+export const PROVIDER_STATUSES = [
+	'tested',
+	'untested',
+	'testing',
+	'missing',
+	'failed'
+] as const;
+export const COPY_TIMEOUT_DURATION = 2000;
