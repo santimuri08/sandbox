832bf4448dcf777e06aeb072f1ab672bb2c7b25e
santimuri08 <115190223+santimuri08@users.noreply.github.com>
2025-11-30 20:00:22 -0500

    Delete src/frontend/eden directory

--- src/frontend/eden/treaty.ts
+++
@@ -1,9 +0,0 @@
-import { treaty } from '@elysiajs/eden';
-import type { Server } from '../../backend/server';
-
-const serverUrl =
-	typeof window !== 'undefined'
-		? window.location.origin
-		: 'http://localhost:3000';
-
-export const server = treaty<Server>(serverUrl);
