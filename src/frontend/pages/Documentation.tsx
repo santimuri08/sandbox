db7b408c1e2c532ca7c82afd19df986693feaf65
santimuri08 <115190223+santimuri08@users.noreply.github.com>
2025-12-03 00:20:30 -0500

    Add files via upload

---
+++ src/frontend/pages/Documentation.tsx
@@ -0,0 +1,54 @@
+import { animated } from '@react-spring/web';
+import { DocsView } from '../../types/types';
+import { Navbar } from '../components/navbar/Navbar';
+import { Head } from '../components/page/Head';
+import { Sidebar } from '../components/sidebar/Sidebar';
+import { docsViews } from '../data/sidebarData';
+import { useAuthStatus } from '../hooks/useAuthStatus';
+import { useDocsNavigation } from '../hooks/useDocsNavigation';
+import { ThemeMode, useTheme } from '../hooks/useTheme';
+import { htmlDefault, bodyDefault, mainDefault } from '../styles/styles';
+
+type DocumentationProps = {
+	theme: ThemeMode | undefined;
+	initialView: DocsView;
+};
+
+export const Documentation = ({ theme, initialView }: DocumentationProps) => {
+	const { user, handleSignOut } = useAuthStatus();
+	const [themeSprings, setTheme] = useTheme(theme);
+	const [view, navigateToView] = useDocsNavigation(initialView);
+
+	const ActiveView = docsViews[view];
+
+	return (
+		<html lang="en" style={htmlDefault}>
+			<Head />
+			<animated.body style={bodyDefault(themeSprings)}>
+				<Navbar
+					setTheme={setTheme}
+					themeSprings={themeSprings}
+					user={user}
+					handleSignOut={handleSignOut}
+				/>
+				<main style={mainDefault('hidden')}>
+					<div
+						style={{
+							display: 'flex',
+							flex: 1,
+							minHeight: 0,
+							overflow: 'hidden'
+						}}
+					>
+						<Sidebar
+							view={view}
+							themeSprings={themeSprings}
+							navigateToView={navigateToView}
+						/>
+						<ActiveView themeSprings={themeSprings} />
+					</div>
+				</main>
+			</animated.body>
+		</html>
+	);
+};
---
+++ src/frontend/pages/Playground.tsx
@@ -0,0 +1,29 @@
+import { animated } from '@react-spring/web';
+import { Navbar } from '../components/navbar/Navbar';
+import { Head } from '../components/page/Head';
+import { PlaygroundPage } from '../components/playground/PlaygroundPage';
+import { useAuthStatus } from '../hooks/useAuthStatus';
+import { ThemeMode, useTheme } from '../hooks/useTheme';
+import { htmlDefault, bodyDefault } from '../styles/styles';
+type PlaygroundProps = {
+	theme: ThemeMode | undefined;
+};
+export const Playground = ({ theme }: PlaygroundProps) => {
+	const { user, handleSignOut } = useAuthStatus();
+	const [themeSprings, setTheme] = useTheme(theme);
+
+	return (
+		<html lang="en" style={htmlDefault}>
+			<Head title="AbsoluteJS Playground" />
+			<animated.body style={bodyDefault(themeSprings)}>
+				<Navbar
+					themeSprings={themeSprings}
+					user={user}
+					handleSignOut={handleSignOut}
+					setTheme={setTheme}
+				/>
+				<PlaygroundPage themeSprings={themeSprings} />
+			</animated.body>
+		</html>
+	);
+};
